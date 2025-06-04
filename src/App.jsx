dff1da3d44da05fb20bfb83267eaea7165d92c72
xiaoxy <xiaoxiaoyu0226@gmail.com>
2025-06-04 12:39:23 -0400

    updated index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// import Component from "@/component.jsx"
+import { BrowserRouter, Route } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return
+  <>
+    <BrowserRouter>
+      <Route>
+        {/* main page route */}
+        <Route index element={<Home />} />
+        {/* other page route 星星的意思就是其他都有路径 凡是没有路径点的都是走这个notfound页面 */}
+        <Route path="*" element={<NotFound />} />
+
+      </Route>
+    </BrowserRouter>
+  </>
 }
 
 export default App
